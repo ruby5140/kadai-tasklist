eb0cd7b30c1cd310d8d698edd846de526bf9c9ee
ruby5140 <day.one_2006@icloud.com>
2017-11-26 10:51:47 +0000

    tasklistapp

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,26 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:new, :create]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-     @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
 
   def show
   end
 
   def new
-     @tasks = Task.new
+     @task = Task.new
   end
 
   def create
-    @tasks = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
-    if @tasks.save
+    if @task.save
       flash[:success] = 'Task が正常に登録されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'Task が登録されませんでした'
       render :new
@@ -29,9 +32,9 @@ class TasksController < ApplicationController
 
   def update
 
-    if @tasks.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -39,7 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @tasks.destroy
+    @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
@@ -49,7 +52,8 @@ class TasksController < ApplicationController
   private
 
   def set_task
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+    redirect_to root_url if @task.user != current_user
   end
 
   def task_params
